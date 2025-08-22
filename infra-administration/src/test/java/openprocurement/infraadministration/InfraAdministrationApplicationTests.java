3070a98afef569ba1421d5b22b2cba73c3270f37
luqq2500 <luqq2500@gmail.com>
2025-08-22 17:41:17 +0800

    .

---
+++ infra-administration/src/test/java/openprocurement/infraadministration/InfraAdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package openprocurement.infraadministration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraAdministrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
