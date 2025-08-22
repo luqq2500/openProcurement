279b188eb7d97d72dd614ef77ce79b760e5da665
luqq2500 <luqq2500@gmail.com>
2025-08-22 16:34:20 +0800

    added administration infra module.

--- infra-identity/src/main/java/message/RegistrationSubmitted_KafkaPublisher.java
+++ infra-identity/src/main/java/message/RegistrationSubmitted_KafkaPublisher.java
@@ -3,17 +3,17 @@ package message;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import identities.registration.events.RegistrationSubmitted;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 import port.IntegrationEventPublisher;
 
+@Service
 public class RegistrationSubmitted_KafkaPublisher implements IntegrationEventPublisher<RegistrationSubmitted> {
     private final KafkaTemplate<String, String> kafkaTemplate;
     private final ObjectMapper objectMapper;
-
     public RegistrationSubmitted_KafkaPublisher(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
         this.kafkaTemplate = kafkaTemplate;
         this.objectMapper = objectMapper;
     }
-
     @Override
     public void publish(RegistrationSubmitted event) {
         try{
